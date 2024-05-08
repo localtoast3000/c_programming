adbc9ff2e03e3146af8d65184d57def664b500ed
Craig <c.chick@tx-consulting.com>
2024-05-08 23:54:36 +0200

    add_two_numbers

--- src/print_args.c
+++ src/print_args.c
@@ -3,11 +3,9 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
+    printf("Executable %s\n", argv[0]);
+
     if (argc > 1) {
-        char *path = argv[0];
-
-        printf("Executable %s\n", path);
-
         for (int i = 1; i < argc; i++) {
             printf("Argument %d: %s\n", i, argv[i]);
         }
