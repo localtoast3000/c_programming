861fbe1f7650c89e605a060bdc339757dfcdfd2b
Craig <c.chick@tx-consulting.com>
2024-05-09 00:45:22 +0200

    Reusable constants header module0

---
+++ lib/error_messages.h
@@ -0,0 +1,6 @@
+#ifndef ERROR_MESSAGES_H
+#define ERROR_MESSAGES_H
+
+static const char ARGS_INVALID[] = "Invalid arguments given";
+
+#endif //ERROR_MESSAGES_H
--- src/split_string.c
+++ src/split_string.c
@@ -3,15 +3,8 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
+    if (argc != 2) {
         printf(ARGS_INVALID);
         return EXIT_FAILURE;
     }
-
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
-
-    printf("%d", a + b);
-
-    return EXIT_SUCCESS;
 }
