5dcf1a72c5fb77e2151681712f64f9fc72be7960
Craig Chick <craig.chick.ext@up.coop>
2024-05-15 09:03:53 +0200

    script styles mac compatible

--- commands/styles.sh
+++ commands/styles.sh
@@ -5,17 +5,17 @@ separator(){
 }
 
 header() {
-    echo -e "\e[0;34m$1\e[0m"  # blue
+    echo -e "\033[0;34m$1\033[0m"  # blue
 }
 
 info() {
-    echo -e "\e[0;33m$1\e[0m"  # yellow
+    echo -e "\033[0;33m$1\033[0m"  # yellow
 }
 
 success() {
-    echo -e "\e[0;32m$1\e[0m"  # green
+    echo -e "\033[0;32m$1\033[0m"  # green
 }
 
 error() {
-    echo -e "\e[0;31m$1\e[0m"  # red
+    echo -e "\033[0;31m$1\033[0m"  # red
 }
